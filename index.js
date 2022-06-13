eb18d3469ed6b570db951ec3ec1aaf5cd8acb900
Dennis Kimathi <kimathidennis38@gmail.com>
2022-06-14 00:00:59 +0300

    commit tree 7691b7c1f7f7d88e914fd4b730a56c94c0834c44

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code goes here
+
+
+
+
+//Create the event
+ const event = document.createEvent("Event");
+ console.log(event);
+
+ //Define the event name as DOMContentload
+event.initEvent('DOMContentload',true,true);
+
+//Listen to the event
+document.addEventListener('DOMContentload', function(){
+    document.querySelector("p").textContent = "This is really cool!";
+    console.log("DOM has loaded!")
+}, true);
+
+const initialText = document.querySelector("p").textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
+console.log(initialText);
+
+console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered")
+
+
+
+//Target can be any element or other event target
+window.document.dispatchEvent(event);
